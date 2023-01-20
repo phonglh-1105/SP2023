34b9b38796c39feb85f7cbfc1ee9f772097f3257
Hong Phong Le <phonghngle@gmail.com>
2023-01-20 22:56:32 +0700

    challenge 2, ex 1,2

--- session 9/index.js
+++ session 9/index.js
@@ -64,3 +64,14 @@ const printGoals = function (...players) {
 printGoals(...game.scored);
 
 team1 < team2 && console.log('Team 1 is more likely to win');
+
+// challenge 2
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
