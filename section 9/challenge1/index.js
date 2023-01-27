5ca924ba81f6e89179d0dee4818cad4153e68bbb
Hong Phong Le <phonghngle@gmail.com>
2023-01-27 15:37:41 +0700

    mkdir challenge 1-2-3-4 and code completion

--- section 9/challenge1/index.js
+++ section 9/challenge1/index.js
@@ -65,24 +65,4 @@ printGoals(...game.scored);
 
 team1 < team2 && console.log("Team 1 is more likely to win");
 
-// challenge 2
-for (const [i, player] of game.scored.entries()) {
-  console.log(`Goal ${i + 1}: ${player}`);
-}
 
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
-
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
-  console.log(`Odd of ${teamStr} ${odd}`);
-}
-
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
-console.log(scorers);
