cf27aa758a8161f30c1399c3975d3c2c1953ee47
Hong Phong Le <phonghngle@gmail.com>
2023-02-08 23:46:54 +0700

    section 11: challenge 1

---
+++ main.js
@@ -0,0 +1,9 @@
+// const isBelowThreshold = (currentValue) => currentValue < 40;
+
+function isBelowThreshold(currentValue) {
+  return currentValue < 40;
+}
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+// Expected output: true
---
+++ section 11/challenge1/index.js
@@ -0,0 +1,13 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+  allDogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
