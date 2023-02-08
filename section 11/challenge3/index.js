1424bf14135ac8a33e5d28d36246acbe6733825b
Hong Phong Le <phonghngle@gmail.com>
2023-02-09 00:07:47 +0700

    section 11: challeng 3

---
+++ section 11/challenge3/index.js
@@ -0,0 +1,5 @@
+const calcAverageHumanAge = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
